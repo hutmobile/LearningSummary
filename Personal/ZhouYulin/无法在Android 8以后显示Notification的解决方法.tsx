a741ae8e7ce5fa7cdec6ac7023843f4d047b1067
Zhouyulin1220 <39131024+Zhouyulin1220@users.noreply.github.com>
2018-05-24 22:14:43 +0800

    Update 无法在Android 8以后显示Notification的解决方法.tsx

--- Personal/ZhouYulin/无法在Android 8以后显示Notification的解决方法.tsx
+++ Personal/ZhouYulin/无法在Android 8以后显示Notification的解决方法.tsx
@@ -3,28 +3,38 @@
 以下是可以同时在8.0版本或者8.0以下版本运用的方法：
 
     public static final String id="id";
-    public static final String name="name";
-    public void onClick(View v) {
+    public static final String name="通知渠道名字";
+       public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_noty:
+                Intent intent=new Intent(this,NotificationActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 if (Build.VERSION.SDK_INT >= 26) {
                     NotificationChannel channel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH);
+                    channel.setDescription("通知渠道的描述");
+                    channel.enableLights(true);//灯光
+                    channel.setLightColor(Color.GREEN);
+                    channel.enableVibration(true);//震动
+                    channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
                     manager.createNotificationChannel(channel);
                     Notification notification = new Notification.Builder(this, id)
                             .setContentTitle("通知")
-                            .setContentText("测试内容测试内容测试内容测试内容API26")
+                            .setContentText("测试内容API26")
                             .setSmallIcon(android.R.drawable.sym_def_app_icon)
                             .setAutoCancel(true)
+                            .setContentIntent(pi)
                             .build();
                     manager.notify(1, notification);
                 }
                 else{
                     Notification notification = new NotificationCompat.Builder(this, id)
                             .setContentTitle("通知")
-                            .setContentText("测试内容测试内容测试内容测试内容")
-                            .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                            .setContentText("测试内容")
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                             .setAutoCancel(true)
+                            .setContentIntent(pi)
                             .build();
                     manager.notify(1, notification);
                 }
